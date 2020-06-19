af75d16bf57b14fbd609777f7b0c1862073a0698
Nydia Valencia <nydia.a.valencia@gmail.com>
2020-06-19 13:29:26 -0500

    Updates

---
+++ precache-manifest.3b49b2290c0d99ef58c7a6271141983a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75a01f65842107d6ce2ac155625a970",
+    "url": "/apod-gallery/index.html"
+  },
+  {
+    "revision": "dd8bbbcea847f51b9878",
+    "url": "/apod-gallery/static/css/2.75a7fdd2.chunk.css"
+  },
+  {
+    "revision": "b8ad47607fce0b4c4d6e",
+    "url": "/apod-gallery/static/css/main.1279e5b3.chunk.css"
+  },
+  {
+    "revision": "dd8bbbcea847f51b9878",
+    "url": "/apod-gallery/static/js/2.a5d013a0.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/apod-gallery/static/js/2.a5d013a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ad47607fce0b4c4d6e",
+    "url": "/apod-gallery/static/js/main.e97d5618.chunk.js"
+  },
+  {
+    "revision": "83595dbd3b30710544f3",
+    "url": "/apod-gallery/static/js/runtime-main.fe4ad272.js"
+  }
+]);
